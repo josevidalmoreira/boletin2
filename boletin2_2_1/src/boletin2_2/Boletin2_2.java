6be505072384028d244b2edb921d25448404879e
jvidalmoreira <jvidalmoreira@victoria20.danielcastelao.org>
2017-10-26 09:30:57 +0200

    inicio

---
+++ boletin2_2_1/src/boletin2_2/Boletin2_2.java
@@ -0,0 +1,12 @@
+
+package boletin2_2;
+
+public class Boletin2_2 {
+
+    public static void main(String[] args) {
+      float lado = 3f, area;
+        area = (float)Math.pow(lado, 2);
+      System.out.println("area ="+area);
+    }
+    
+}
